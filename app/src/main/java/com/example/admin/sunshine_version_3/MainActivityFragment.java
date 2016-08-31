9eaf2805ee1842487b8e6cd5b318581953c2fb8b
Richmond Afeawo <Rafeawo@gmail.com>
2016-08-31 23:55:13 +0100

    Add list items to fragment activity

--- app/src/main/java/com/example/admin/sunshine_version_3/MainActivityFragment.java
+++ app/src/main/java/com/example/admin/sunshine_version_3/MainActivityFragment.java
@@ -17,6 +17,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Cloudy - 88/63",
+                "Wednesday - Foggy - 88/63",
+                "Thursday - Sunny - 88/63",
+                "Friday - Heavy Rain - 88/63",
+                "Saturday - Hail - 88/63",
+                "Sunday - Snow Storm :'( - 88/63"
+        };
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
