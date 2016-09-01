b750f359af10462289aa1738f426a2fd3669f78a
Richmond Afeawo <Rafeawo@gmail.com>
2016-09-01 23:17:07 +0100

    Set ArrayAdapter on list view

--- app/src/main/java/com/example/admin/sunshine_version_3/MainActivityFragment.java
+++ app/src/main/java/com/example/admin/sunshine_version_3/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,6 +20,8 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
         String[] forecastArray = {
                 "Today - Sunny - 88/63",
                 "Tomorrow - Cloudy - 88/63",
@@ -29,12 +32,16 @@ public class MainActivityFragment extends Fragment {
                 "Sunday - Snow Storm :'( - 88/63"
         };
 
-        new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.list_item_forecast,
-                R.id.list_item_forecast_textview,
-                forecastArray);
+        ArrayAdapter<String> mForecastAdapter =
+            new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    forecastArray);
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
